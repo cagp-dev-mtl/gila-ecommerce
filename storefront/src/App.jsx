3acc191e3f954485c70d6c534ddc3ecee5046532
Carlos Gutierrez <carlos.perez@altitude-sports.com>
2026-09-16 01:51:55 -0600

    feat(gila-07): csv import page

--- storefront/src/App.jsx
+++ storefront/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 
 import Layout from './components/Layout'
+import ImportPage from './pages/ImportPage'
 import ProductForm from './pages/ProductForm'
 import ProductList from './pages/ProductList'
 
@@ -9,6 +10,7 @@ function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<ProductList />} />
+        <Route path="/import" element={<ImportPage />} />
         <Route path="/products/new" element={<ProductForm />} />
         <Route path="/products/:id/edit" element={<ProductForm />} />
       </Routes>
